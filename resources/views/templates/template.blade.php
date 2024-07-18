e2bee74db62d2f1d04658a04d790b1d619ec5a14
Miguel Olarte <lmolarteb@unadvirtual.edu.co>
2024-07-18 02:26:12 -0500

    Se agrega Tailwind

--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -1,16 +1,17 @@
 <head>
-    <link rel="stylesheet" href="{{asset('css/styles.css')}}">
+    <!-- <link rel="stylesheet" href="{{asset('css/styles.css')}}"> -->
+    @vite(['resources/css/app.css'])
 </head>
 
-<div class="template">
-    <p>Santa-R</p>
-    <nav>
-        <ul>
-            <li><a href="{{route('productos')}}">Productos</a></li>
-            <li><a href="{{route('inventario')}}">Inventario</a></li>
-            <li><a href="{{route('compras')}}">Compras</a></li>
-            <li><a href="{{route('ventas')}}">Ventas</a></li>
-            <li><a href="{{route('ingredientesCostos')}}">Costos</a></li>
+<div class="flex items-center p-6 w-auto bg-sky-700">
+    <p class="w-1/4 text-7xl text-white">Santa-R</p>
+    <nav class="w-2/4">
+        <ul class="flex items-center p-2 space-x-36">
+            <li class=" text-2xl hover:text-white"><a href="{{route('productos')}}" class="">Productos</a></li>
+            <li class=" text-2xl hover:text-white"><a href="{{route('inventario')}}">Inventario</a></li>
+            <li class=" text-2xl hover:text-white"><a href="{{route('compras')}}">Compras</a></li>
+            <li class=" text-2xl hover:text-white"><a href="{{route('ventas')}}">Ventas</a></li>
+            <li class=" text-2xl hover:text-white"><a href="{{route('ingredientesCostos')}}">Costos</a></li>
         </ul>
     </nav>
 </div>
